6a2b705946ae9c3f9529b87f1a1fe2516f13e8d4
Gabriel de Freitas <gabrieldfreitas9@gmail.com>
2018-01-06 18:35:11 -0200

    conectado componente billing cycle ao redux para chamar método de inicializacao selecionando automaticamente o tabList

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
@@ -8,7 +11,14 @@ import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 
+import { selectTab } from '../common/tab/tabActions'
+
 class BillingCycle extends Component {
+
+   componentWillMount() {
+      this.props.selectTab('tabList')
+   }
+
    render() {
       return (
          <div>
@@ -42,4 +52,6 @@ class BillingCycle extends Component {
    }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+
+export default connect(null, mapDispatchToProps)(BillingCycle)
