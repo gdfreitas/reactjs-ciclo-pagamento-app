21af92b728ad308a70cf924e9bb3ed45bd95d525
Gabriel de Freitas <gabrieldfreitas9@gmail.com>
2018-01-06 16:28:43 -0200

    aplicado componente de valuebox no dashboard e criado componente row para facilitar alinhamento de grid do dashboard

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,13 +2,21 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from  '../common/layout/row'
 
 class Dashboard extends Component {
    render() {
       return (
          <div>
             <ContentHeader title="Dashboard" small="Versão 1.0" />
-            <Content>Dashboard</Content>
+            <Content>
+               <Row>
+                  <ValueBox cols="12 4" color="green" icon="bank" value="R$ 10" text="Total de créditos" />
+                  <ValueBox cols="12 4" color="red" icon="bank" value="R$ 10" text="Total de débitos" />
+                  <ValueBox cols="12 4" color="blue" icon="bank" value="R$ 0" text="Valor consolidado" />
+               </Row>
+            </Content>
          </div>
       )
    }
