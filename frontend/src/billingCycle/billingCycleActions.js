32bcfe53d0ae7a27a3a25f4ee514b9c555f95364
Gabriel de Freitas <gabrieldfreitas9@gmail.com>
2018-01-06 23:19:22 -0200

    implementado o  disparar de multiplas actions após criação como resetar formulario, atualizar lista, selecionar a tabList, e exibir tabList e tabCreate

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -12,14 +14,19 @@ export function getList() {
 }
 
 export function create(values) {
-   axios.post(`${BASE_URL}/billingCycles`, values)
-      .then(resp => {
-         toastr.success('Sucesso!', 'Operação realizada com sucesso.')
-      })
-      .catch(e => {
-         e.response.data.errors.forEach(error => toastr.error('Erro', error))
-      })
-   return {
-      type: 'TEMP'
+   return dispatch => {
+      axios.post(`${BASE_URL}/billingCycles`, values)
+         .then(resp => {
+            toastr.success('Sucesso!', 'Operação realizada com sucesso.')
+            dispatch([
+               resetForm('billingCycleForm'),
+               getList(),
+               selectTab('tabList'),
+               showTabs('tabList', 'tabCreate')
+            ])
+         })
+         .catch(e => {
+            e.response.data.errors.forEach(error => toastr.error('Erro', error))
+         })
    }
 }
