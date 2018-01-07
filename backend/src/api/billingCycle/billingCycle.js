122731634a44313661ba1766aa7fd95a99539f55
Gabriel de Freitas <gabrieldfreitas9@gmail.com>
2018-01-06 22:52:45 -0200

    correções mensagens de validação da api do backend

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -2,20 +2,20 @@ const restful = require('node-restful') // forma simples de expor uma api rest p
 const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true }
+    name: { type: String, required: [true, 'É obrigatório informar a descrição do crédito'] },
+    value: { type: Number, min: 0, required: [true, 'É obrigatório informar o valor'] }
 })
 
 const debtSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: [true, 'É obrigatório informar a descrição do débito'] },
     value: { type: Number, min: 0, required: [true, 'É obrigatório informar o valor do débito'] },
     status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
 const billingCycleSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    month: { type: Number, min: 1, max: 12, required: true },
-    year: { type: Number, min: 1970, max: 2100, required: true },
+    name: { type: String, required: [true, 'É obrigatório informar o nome do ciclo de pagamento'] },
+    month: { type: Number, min: 1, max: 12, required: [true, 'É obrigatório informar o mês'] },
+    year: { type: Number, min: 1970, max: 2100, required: [true, 'É obrigatório informar o ano'] },
     credits: [creditSchema],
     debts: [debtSchema]
 })
